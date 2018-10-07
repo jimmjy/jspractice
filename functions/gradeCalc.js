55d646435113fb2765bffb7da3fca2e6a3daf29d
James Finkelstein <jfinkelstein9@me.com>
2018-10-07 07:18:56 -0400

    try catch updates

--- functions/gradeCalc.js
+++ functions/gradeCalc.js
@@ -3,8 +3,13 @@
 // 15/20 -> You got a C (75%)
 // a 90-100, b 80 - 89, c 70 -79, d 60 - 69, f 0 -59
 
-const studentsGrade = function (score, total) {
-    const percent = score / total * 100;
+const studentsGrade = function (score, totalScore) {
+
+    if (typeof score !== 'number' && typeof totalScore !== 'number') {
+        throw Error ('Please put in a number');
+    } 
+
+    const percent = score / totalScore * 100;
 
     if (percent <= 59 && percent > 0) {
         return `You got an F (${percent})`;
@@ -14,13 +19,18 @@ const studentsGrade = function (score, total) {
         return `You got a C (${percent})`;
     } else if (percent <= 89) {
         return `You got a B (${percent})`;
-    } else if(percent <= 100) {
+    } else if (percent <= 100) {
         return `You got an A (${percent}), good job!`;
     } else {
         return 'Please put in right scores';
     }
+
 }
 
-const grade = studentsGrade(13, 20);
-
-console.log(grade);
+try {
+    
+    const grade = studentsGrade(12, 20);
+    console.log(grade);
+} catch (e) {
+    console.log(e.message);
+}
