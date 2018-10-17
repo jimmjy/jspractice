d41bc329d7cd9d40b83e78dc5e5994d9a26ca795
James Finkelstein <jfinkelstein9@me.com>
2018-10-17 18:18:47 -0400

    removed and updated

--- oop/person.js
+++ oop/person.js
@@ -1,28 +1,47 @@
-const Person = function (firstName, lastName, age, likes = []) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.likes = likes;
-};
+class Person {
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.likes = likes;
+    }
 
-Person.prototype.getBio = function () {
-    let bio = `${this.firstName} is ${this.age}.`;
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`;
 
-    this.likes.forEach((like) => {
-        bio += ` ${this.firstName} likes ${like}.`;
-    });
-    return bio;
-};
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`;
+        });
+        return bio;
+    }
 
-Person.prototype.setName = function (fullName) {
-    const names = fullName.split(' ');
-    this.firstName = names[0];
-    this.lastName = names[1];
+    setName(fullName) {
+        const names = fullName.split(' ');
+        this.firstName = names[0];
+        this.lastName = names[1];
+    }
 }
 
-const me = new Person('James', 'Finkelstein', 30, ['Biking', 'Coding']);
-me.setName('Alexis Sanders');
+class Employee extends Person {
+    constructor(firstName, lastName, age, likes, job) {
+        super(firstName, lastName, age, likes);
+        this.job = job;
+    };
+
+    getBio() {
+        return `${this.firstName} ${this.lastName} is a ${this.job}.`;
+    };
+
+    getYearsLeft() {
+        return 65 - this.age;
+    };
+};
+
+
+const me = new Employee('James', 'Finkelstein', 30, ['Biking', 'Coding'], 'Web Developer');
+me.setName('Alexis Turner');
 console.log(me.getBio());
+console.log(me.getYearsLeft());
 
 
 const person2 = new Person('Clancy', 'Turner', 51);
