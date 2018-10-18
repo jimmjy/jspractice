0191c94628e6d2c009a1447b4066300c97beab41
James Finkelstein <jfinkelstein9@me.com>
2018-10-17 22:48:33 -0400

    getters and setters

--- oop/person.js
+++ oop/person.js
@@ -15,10 +15,14 @@ class Person {
         return bio;
     }
 
-    setName(fullName) {
+    set fullName (fullName) {
         const names = fullName.split(' ');
         this.firstName = names[0];
         this.lastName = names[1];
+    };
+
+    get fullName () {
+        return `${this.firstName} ${this.lastName}`;
     }
 }
 
@@ -29,11 +33,46 @@ class Employee extends Person {
     };
 
     getBio() {
-        return `${this.firstName} ${this.lastName} is a ${this.job}.`;
+        return `${this.fullName} is a ${this.job}.`;
     };
 
     getYearsLeft() {
         return 65 - this.age;
     };
 };
+
+class Student extends Person {
+    constructor(firstName, lastName, age, grade) {
+        super(firstName, lastName, age);
+        this.grade = grade;
+    };
+
+    getBio() {
+        const message = this.grade >= 70 ? `${this.firstName} ${this.lastName} is passing the course` : `${this.firstName} ${this.lastName} is failing the course`;
+        return message;
+    };
+
+    updateGrade(grade) {
+        this.grade += grade;
+    };
+};
+
+const Test = function (firstName) {
+    this.firstName = firstName;
+}
+
+const test = new Test("James");
+console.log(test);
+
+const me = new Employee('James', 'Fink', 30, ['reading'], 'Student');
+me.fullName = 'Caterina Gennero';
+console.log(me.getBio());
+
+// me.updateGrade(20);
+// console.log(me.getBio());
+// me.updateGrade(-30);
+// console.log(me.getBio());
+
+const kim = new Employee('Kim', 'Charters', 25, ['biking', 'weed'], 'Event Planner');
+console.log(kim.getBio());
 
