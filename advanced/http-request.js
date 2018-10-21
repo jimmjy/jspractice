8abdec2db610eb66fe7c068e55a89493731e476e
James Finkelstein <jfinkelstein9@me.com>
2018-10-21 13:17:45 -0400

    added comments to http

--- advanced/http-request.js
+++ advanced/http-request.js
@@ -36,3 +36,21 @@ requestTwo.addEventListener('readystatechange', (e) => {
         document.getElementById('test').textContent = 'Loading';
     };
 });
+
+// const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
+
+//     // http request
+//     const request = new XMLHttpRequest();
+
+//     request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
+//     request.send();
+
+//     request.addEventListener('readystatechange', (e) => {
+//         if (e.target.readyState === 4 && e.target.status === 200) {
+//             const data = JSON.parse(e.target.responseText);
+//             resolve(data.puzzle);
+//         } else if (e.target.readyState === 4) {
+//             reject('Unable to find words for puzzle');
+//         }
+//     });
+// });
