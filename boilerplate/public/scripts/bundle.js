a8a6df201bc90e5169193a71a90cb1fe7d9889aa
James Finkelstein <jfinkelstein9@me.com>
2018-11-08 12:10:50 -0500

    added babel

---
+++ boilerplate/public/scripts/bundle.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var name = 'Jen';
+console.log(name);
+
+var Hangman = function () {
+    function Hangman() {
+        _classCallCheck(this, Hangman);
+    }
+
+    _createClass(Hangman, [{
+        key: 'myMethod',
+        value: function myMethod() {
+            return 'Testing';
+        }
+    }, {
+        key: 'myNewMethod',
+        value: function myNewMethod() {
+            return 'Hello';
+        }
+    }]);
+
+    return Hangman;
+}();
+
+;
+
+var hangman = new Hangman();
+console.log(hangman.myMethod());
+console.log(hangman.myNewMethod());
+
+var t = function () {
+    var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+        var request, data;
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+            while (1) {
+                switch (_context.prev = _context.next) {
+                    case 0:
+                        _context.next = 2;
+                        return fetch('https://jsonplaceholder.typicode.com/todos/1');
+
+                    case 2:
+                        request = _context.sent;
+                        _context.next = 5;
+                        return request.json;
+
+                    case 5:
+                        data = _context.sent;
+                        return _context.abrupt('return', data);
+
+                    case 7:
+                    case 'end':
+                        return _context.stop();
+                }
+            }
+        }, _callee, undefined);
+    }));
+
+    return function t() {
+        return _ref.apply(this, arguments);
+    };
+}();
+
+console.log(t());
